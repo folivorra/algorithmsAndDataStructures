324b6c1964ce000af4fe41c3c8923308e600139e
Emil Yay <keyt.isac@yandex.ru>
2025-03-17 02:49:23 +0300

    allcups3

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"math"
+)
 
+func main() {
+	n := 0
+	fmt.Scanf("%d", &n)
+	arr := make([]int, n)
+	for i := 0; i < n; i++ {
+		fmt.Scan(&arr[i])
+	}
+	max := math.MinInt
+	for _, v := range arr {
+		if v >= max {
+			max = v
+		}
+	}
+	fmt.Print(max)
 }
