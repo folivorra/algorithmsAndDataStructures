406898f875b5d8b4f96416c4bf945c3c0dcb7997
Emil Yay <keyt.isac@yandex.ru>
2025-05-21 05:13:25 +0300

    feat(studyDir): added sync.Cond exmpls and move funcs to another file

--- dirForStudy/ctx.go
+++ dirForStudy/ctx.go
@@ -3,12 +3,10 @@ package dirForStudy
 import (
 	"context"
 	"fmt"
-	"golang.org/x/sync/errgroup"
 	"math/rand"
 	"os"
 	"os/signal"
 	"sync"
-	"sync/atomic"
 	"time"
 )
 
@@ -120,72 +118,4 @@ func workerSignal(ctx context.Context, id int, wg *sync.WaitGroup) {
 			time.Sleep(1 * time.Second)
 		}
 	}
-}
-
-func ErrorGroup() {
-	rand.New(rand.NewSource(time.Now().UnixNano()))
-	group, ctx := errgroup.WithContext(context.Background())
-	// функция создает группу и контекст для этой группы, который отменится если кто то из группы завершится с ошибкой
-
-	for i := 0; i < 5; i++ {
-		i := i
-		// защита от замыкания
-		group.Go(func() error {
-			select {
-			case <-time.After(time.Second * time.Duration(rand.Intn(5)+1)):
-				if i == rand.Intn(5) {
-					return fmt.Errorf("worker %d finished with error", i)
-				}
-				// имитируем случайную ошибку и возвращаем ее -> отменяем контекст группы
-
-				fmt.Printf("worker %d done his job\n", i)
-				// если ошибки не было значит воркер выполнил свою работу
-				return nil
-			case <-ctx.Done():
-				// когда контекст группы отменяется - этот канал закрывается и мы возвращаем ошибку завершения группы
-				return ctx.Err()
-			}
-		})
-	}
-
-	// ждем выполнение всех горутин в группе и если ошибка ненулевая, то выводим ее, иначе все ок
-	if err := group.Wait(); err != nil {
-		fmt.Println("error:", err)
-	} else {
-		fmt.Println("worker finished without error")
-	}
-}
-
-// атомики это примитивы синхронизации обеспечивающие атомарные операции с данными (чтение/запись),
-// они менее прожорливые чем мьютексы, но используются в простых структурах где необходимо
-// менять лишь одно поле (например, счетчик)
-
-type Counter struct {
-	value int64
-}
-
-func (c *Counter) Increment() {
-	atomic.AddInt64(&c.value, 1)
-}
-
-func (c *Counter) Get() int64 {
-	return atomic.LoadInt64(&c.value)
-}
-
-func AtomicGoroutine() {
-	wg := &sync.WaitGroup{}
-	c := &Counter{}
-
-	for i := 0; i < 100; i++ {
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			for j := 0; j < 100; j++ {
-				c.Increment()
-			}
-		}()
-	}
-
-	wg.Wait()
-	fmt.Printf("counter %d\n", c.Get())
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module algoAndDS
 
-go 1.22
+go 1.23.0
+
+toolchain go1.23.7
+
+require golang.org/x/sync v0.14.0 // indirect
